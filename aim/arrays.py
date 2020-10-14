81c7490355de9ac70dddff518415e13c37626a45
Susensio <susensio@desktop>
2020-10-14 07:36:21 +0200

    Add rows and columns helpers

--- aim/arrays.py
+++ aim/arrays.py
@@ -10,6 +10,32 @@ class Matrix(list):
 
         return super().__init__(iterable)
 
+    def row(self, row_num):
+        return self[row_num, :]
+
+    def column(self, col_num):
+        return self[:, col_num]
+
+    @property
+    def m(self):
+        """Number of rows."""
+        return len(self.column(0))
+
+    @property
+    def n(self):
+        """Number of columns."""
+        return len(self.row(0))
+
+    @property
+    def rows(self):
+        for row_num in range(self.m):
+            yield self.row(row_num)
+
+    @property
+    def columns(self):
+        for col_num in range(self.n):
+            yield self.column(col_num)
+
     def __getitem__(self, arg):
         rows, cols = arg
         try:
--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -7,6 +7,11 @@ def simple_matrix():
     return Matrix([[1, 2], [3, 4]])
 
 
+@pytest.fixture
+def rectangular_matrix():
+    return Matrix([[1, 2], [3, 4], [5, 6]])
+
+
 def test_slice_row(simple_matrix):
     assert simple_matrix[0, :] == [1, 2]
 
@@ -31,3 +36,16 @@ def test_matrix_is_iterable():
     with pytest.raises(TypeError):
         Matrix(None)
         Matrix([None])
+
+
+def test_matrix_dimmensions(rectangular_matrix):
+    assert rectangular_matrix.m == 3
+    assert rectangular_matrix.n == 2
+
+
+def test_matrix_columns(simple_matrix):
+    assert list(simple_matrix.columns) == [[1, 3], [2, 4]]
+
+
+def test_matrix_row(simple_matrix):
+    assert list(simple_matrix.row(1)) == [3, 4]
